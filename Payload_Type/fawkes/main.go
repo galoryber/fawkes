6151bde5b43d7dcdc51de49fcc9b039e520a978a
Gary Lobermier <gary.lobermier@globetech.biz>
2024-11-26 14:37:58 -0600

    asdf

--- Payload_Type/fawkes/main.go
+++ Payload_Type/fawkes/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 
-	payloadService := structs.AllPayloadData.Get("fawkesAgent")
+	payloadService := structs.AllPayloadData.Get("basicAgent")
 
 	// Build the Payload container definition and add it
 	// If running as standalone, locally, outside Mythic: export MYTHIC_SERVER_HOST=127.0.0.1
