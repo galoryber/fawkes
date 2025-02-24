2e948875010b56ab3e0e7cd31989071e0f299d1d
Gary Lobermier <gary.lobermier@globetech.biz>
2025-02-24 15:17:26 -0600

    asdf

---
+++ Payload_Type/fawkes/basic_agent/agent_code/sleep.go
@@ -0,0 +1,43 @@
+package sleep
+
+import (
+	// Standard
+	"encoding/json"
+
+	"github.com/MythicAgents/freyja/Payload_Type/freyja/agent_code/pkg/profiles"
+
+	// Freyja
+
+	"github.com/MythicAgents/freyja/Payload_Type/freyja/agent_code/pkg/utils/structs"
+)
+
+type Args struct {
+	Interval int `json:"interval"`
+	Jitter   int `json:"jitter"`
+}
+
+// Run - interface method that retrieves a process list
+func Run(task structs.Task) {
+	args := Args{}
+	err := json.Unmarshal([]byte(task.Params), &args)
+	if err != nil {
+		errResp := task.NewResponse()
+		errResp.SetError(err.Error())
+		task.Job.SendResponses <- errResp
+		return
+	}
+	output := ""
+	if args.Interval >= 0 {
+		output += profiles.UpdateAllSleepInterval(args.Interval)
+	}
+	if args.Jitter >= 0 && args.Jitter <= 100 {
+		output += profiles.UpdateAllSleepJitter(args.Jitter)
+	}
+	msg := task.NewResponse()
+	msg.UserOutput = output
+	sleepString := profiles.GetSleepString()
+	msg.ProcessResponse = &sleepString
+	msg.Completed = true
+	task.Job.SendResponses <- msg
+	return
+}
