fbe6364c8b5a2eaf216c6fa6f0cfe999ba5f4378
Gary Lobermier <gary.lobermier@globetech.biz>
2026-01-04 21:02:49 -0600

    ps command

--- Payload_Type/fawkes/fawkes/agentfunctions/ps.go
+++ Payload_Type/fawkes/fawkes/agentfunctions/ps.go
@@ -7,8 +7,8 @@ import (
 func init() {
 	agentstructs.AllPayloadData.Get("fawkes").AddCommand(agentstructs.Command{
 		Name:                "ps",
-		Description:         "ps - List running processes with details",
-		HelpString:          "ps",
+		Description:         "ps [-v] [filter] - List running processes. Use -v for verbose output with command lines. Optional filter to search by process name.",
+		HelpString:          "ps [-v] [filter]",
 		Version:             1,
 		MitreAttackMappings: []string{"T1057"}, // Process Discovery
 		SupportedUIFeatures: []string{},
@@ -17,17 +17,24 @@ func init() {
 			SupportedOS: []string{agentstructs.SUPPORTED_OS_LINUX, agentstructs.SUPPORTED_OS_MACOS, agentstructs.SUPPORTED_OS_WINDOWS},
 		},
 		TaskFunctionParseArgString: func(args *agentstructs.PTTaskMessageArgsData, input string) error {
-			// ps takes no arguments
+			// Pass the input as-is (can be empty, "-v", "processname", or "-v processname")
+			if input != "" {
+				args.SetManualArgs(input)
+			}
 			return nil
 		},
 		TaskFunctionParseArgDictionary: func(args *agentstructs.PTTaskMessageArgsData, input map[string]interface{}) error {
-			// ps takes no arguments
-			return nil
+			// Support JSON input if needed
+			return args.LoadArgsFromDictionary(input)
 		},
 		TaskFunctionCreateTasking: func(task *agentstructs.PTTaskMessageAllData) agentstructs.PTTaskCreateTaskingMessageResponse {
 			response := agentstructs.PTTaskCreateTaskingMessageResponse{
 				Success: true,
 				TaskID:  task.Task.ID,
+			}
+			// Display parameters if any
+			if displayParams, err := task.Args.GetFinalArgs(); err == nil && displayParams != "" {
+				response.DisplayParams = &displayParams
 			}
 			return response
 		},
